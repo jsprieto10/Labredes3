18203ee9d8a5abea76c0bba31fb3e6093a55d076
Juan Sebastian Prieto <js.prieto10@uniandes.edu.co>
2019-03-22 16:28:18 -0500

    cambio ip waira

--- Client/clientTCP.py
+++ Client/clientTCP.py
@@ -5,7 +5,7 @@ import os
 import time
 import client
 
-serverName = '127.0.0.1'
+serverName = '157.253..222.174'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
